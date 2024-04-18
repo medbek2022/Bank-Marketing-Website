a40680f0e4d965bf6939c7ca74d952dc0a1b90af
Bekour Mohamed <mohamed.bekour@stud.hs-emden-leer.de>
2024-04-18 09:50:46 +0200

    Home page done

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,15 @@
-'use strict'
+"use strict";
+
+//****************** Sctipt Start ******************
+//---Header---
+const header = document.getElementById("header");
+const scrolToBtn = header.querySelector(".link");
+//---section 1---
+const section1 = document.getElementById("section_1");
+
+// ****************** Sctipt Start ******************
+// ---Smooth Scroling---
+scrolToBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  section1.scrollIntoView({ behavior: "smooth" });
+});
